e658be26e0d0819e7bc55800c43d58a9217bf83d
Anand Rana <anandr836@gmail.com>
2018-11-13 21:37:20 +0530

    project

---
+++ enrollx-html/js/hideshow.js
@@ -0,0 +1,110 @@
+  $(document).ready(function () {
+    $("#hide").click(function () {
+        $("#hide-data").hide(200);
+    });
+    $("#show").click(function () {
+        $("#hide-data").show("slide");
+    });	
+    $("#hide1").click(function () {
+        $("#hide-data1").hide(200);
+    });
+    $("#show1").click(function () {
+        $("#hide-data1").show(200);
+    });	
+    $("#hide2").click(function () {
+        $("#hide-data2").hide(200);
+    });
+    $("#show2").click(function () {
+        $("#hide-data2").show(200);
+    });	
+    $("#hide3").click(function () {
+        $("#hide-data3").hide(200);
+    });
+    $("#show3").click(function () {
+        $("#hide-data3").show(200);
+    });	
+    $("#hide4").click(function () {
+        $("#hide-data4").hide(200);
+    });
+    $("#show4").click(function () {
+        $("#hide-data4").show(200);
+    });	
+    $("#hide5").click(function () {
+        $("#hide-data5").hide(200);
+    });
+    $("#show5").click(function () {
+        $("#hide-data5").show(200);
+    });	
+    $("#hide6").click(function () {
+        $("#hide-data6").hide(200);
+    });
+    $("#show6").click(function () {
+        $("#hide-data6").show(200);
+    });	
+    $("#hide7").click(function () {
+        $("#hide-data7").hide(200);
+    });
+    $("#show7").click(function () {
+        $("#hide-data7").show(200);
+    });	
+    $("#hide8").click(function () {
+        $("#hide-data8").hide(200);
+    });
+    $("#show8").click(function () {
+        $("#hide-data8").show(200);
+    });	
+    $("#hide9").click(function () {
+        $("#hide-data9").hide(200);
+    });
+    $("#show9").click(function () {
+        $("#hide-data9").show(200);
+    });	
+    $("#hide10").click(function () {
+        $("#hide-data10").hide(200);
+    });
+    $("#show10").click(function () {
+        $("#hide-data10").show(200);
+    });	
+    $("#hide11").click(function () {
+        $("#hide-data11").hide(200);
+    });
+    $("#show11").click(function () {
+        $("#hide-data11").show(200);
+    });	
+    $("#hide12").click(function () {
+        $("#hide-data12").hide(200);
+    });
+    $("#show12").click(function () {
+        $("#hide-data12").show(200);
+    });	
+    $("#hide13").click(function () {
+        $("#hide-data13").hide(200);
+    });
+    $("#show13").click(function () {
+        $("#hide-data13").show(200);
+    });	
+    $("#hide14").click(function () {
+        $("#hide-data14").hide(200);
+    });
+    $("#show14").click(function () {
+        $("#hide-data14").show(200);
+    });	
+    $("#hide15").click(function () {
+        $("#hide-data15").hide(200);
+    });
+    $("#show15").click(function () {
+        $("#hide-data15").show(200);
+    });	
+    $("#hide16").click(function () {
+        $("#hide-data16").hide(200);
+    });
+    $("#show16").click(function () {
+        $("#hide-data16").show(200);
+    });	
+    $("#hide17").click(function () {
+        $("#hide-data17").hide(200);
+    });
+    $("#show17").click(function () {
+        $("#hide-data17").show(200);
+    });	
+  });	
